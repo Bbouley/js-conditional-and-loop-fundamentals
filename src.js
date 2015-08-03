c3627d2515f6078372a3da02d4d4ad21871cc46b
bbouley <b.bouley@hotmail.co.uk>
2015-08-03 11:48:20 -0600

    first two tests passed

--- src.js
+++ src.js
@@ -1,7 +1,26 @@
 var yourCode = {
-  commafy: function(n) {},
 
-  joinToString: function(array) {}
-}
+  commafy: function(n) {
+    var start = '';
+    for (var i = 1; i <= n; i++) {
+      if (i === n) {
+        start = start.concat(i);
+      } else {
+      start = start.concat(i + ',');
+      }
+    }
+    return start;
+  },
+
+  joinToString: function(array) {
+      var newArray = array.join(' and ');
+      return newArray + (' oh my.');
+    },
+
+  printNumbers: function(num){
+    return 'test';
+  }
+
+};
 
 module.exports = yourCode;
